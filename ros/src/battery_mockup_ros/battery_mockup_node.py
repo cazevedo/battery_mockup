64971fbe80a1b91ee40e31a642e14567c14f3331
Carlos Azevedo <cmaz90@gmail.com>
2021-06-21 10:56:52 +0100

    fixed package for noetic

--- ros/src/battery_mockup_ros/battery_mockup_node.py
+++ ros/src/battery_mockup_ros/battery_mockup_node.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import rospy
 import sensor_msgs.msg as sensor_msgs
 from std_msgs.msg import Bool
@@ -45,20 +46,36 @@ class Battery:
         else:
             self.battery_autonomy = 30
 
-        print('Battery autonomy: ', self.battery_autonomy)
+        rospy.loginfo('Battery autonomy: %f', self.battery_autonomy)
 
         # battery half life and total life
         self.bat_half_life = (10.0-np.log(1))/(np.log(2)/(self.battery_autonomy/2)*10)
         self.bat_max_life = 2*self.bat_half_life
 
         # Set the initial battery level
+        initial_battery_level = 'full'
         # Discharged
-        # self.seconds_elapsed = 0
+        if initial_battery_level == 'empty':
+            self.seconds_elapsed = 0
+        # Half Charged
+        elif initial_battery_level == 'half':
+            self.seconds_elapsed = self.bat_half_life
+        # Fully Charged
+        elif initial_battery_level == 'full':
+            self.seconds_elapsed = self.bat_max_life
         # Choice between 0, half or fully charged
-        # self.seconds_elapsed = np.random.choice([0, self.bat_half_life, self.bat_max_life])
+        elif initial_battery_level == 'random_choice':
+            self.seconds_elapsed = np.random.choice([0, self.bat_half_life, self.bat_max_life])
         # Uniform distribution
-        self.seconds_elapsed = np.random.uniform(0, self.bat_max_life)
-        print('Seconds chosen : ', self.seconds_elapsed)
+        elif initial_battery_level == 'random_uniform':
+            self.seconds_elapsed = np.random.uniform(0, self.bat_max_life)
+
+        rospy.loginfo('Seconds chosen : %f', self.seconds_elapsed)
+
+        # Charging speed (factor multiplied by each unit of time)
+        self.chargind_speed = 1
+        # Dischargind speed (factor multiplied by each unit of time)
+        self.dischargind_speed = 1
 
         self.white_noise = False
 
@@ -66,20 +83,21 @@ class Battery:
     def battery_charge(self, time, battery_autonomy):
         if self.white_noise:
             battery_autonomy = np.random.normal(loc=battery_autonomy, scale=3)
-        lb = np.log(2)/(battery_autonomy/2)*10
+        lb = np.log(2)/(battery_autonomy/2)*10.0
         return 1/(1+np.exp(-lb*time+10))
 
     # Battery discharge model
     def battery_discharge(self, time, battery_autonomy):
         if self.white_noise:
             battery_autonomy = np.random.normal(loc=battery_autonomy, scale=3)
-        lb = np.log(2)/(battery_autonomy/2)*10
+        lb = np.log(2)/(battery_autonomy/2)*10.0
         return 1/-(1+np.exp(-lb*time+10))+1
 
     # Called when robot changes state, from charging to discharging or vice-versa
     def chargingCallback(self, charging_state):
         if self.is_charging != bool(charging_state.data):
             self.is_charging = bool(charging_state.data)
+
             # mirror the time elapsed to obtain the exact correspondence between the discharge and charge model timestamp
             self.seconds_elapsed = self.bat_half_life+(self.bat_half_life-self.seconds_elapsed)
 
@@ -90,20 +108,22 @@ class Battery:
         """
         Spin around, updating battery state and publishing the result.
         """
-        rate = rospy.Rate(1)  # hz
+        rate = rospy.Rate(1)  # Hz
         while not rospy.is_shutdown():
             if self.is_charging:
                 self.battery.percentage = min(1, self.battery_charge(self.seconds_elapsed, self.battery_autonomy))
                 self.battery.power_supply_status = sensor_msgs.BatteryState.POWER_SUPPLY_STATUS_CHARGING
+
+                self.seconds_elapsed = min(self.bat_max_life, self.seconds_elapsed+self.chargind_speed)
             else:
                 self.battery.percentage = max(0, self.battery_discharge(self.seconds_elapsed, self.battery_autonomy))
                 self.battery.power_supply_status = sensor_msgs.BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
 
+                self.seconds_elapsed = min(self.bat_max_life, self.seconds_elapsed+self.dischargind_speed)
+
             self.battery.header.stamp = rospy.get_rostime()
             self.battery_publisher.publish(self.battery)
             rate.sleep()
-
-            self.seconds_elapsed = min(self.bat_max_life, self.seconds_elapsed+1)
 
 def main():
     battery = Battery()
